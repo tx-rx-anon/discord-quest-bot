fdde28c80846e68fd4a8ffa506408cba30b4d9c4
tx-rx-anon <tx-rx-anon@users.noreply.github.com>
2020-07-02 16:31:51 -0400

    New command interface and skeleton quest management.

--- bot.js
+++ bot.js
@@ -1,291 +1,227 @@
+///////////
+// Setup //
+///////////
+
 // Run dotenv
 require('dotenv').config();
 
 // Import libraries
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const QuestBot = require('./lib');
+const winston = require('winston');
 
-let PREFIX = "%";
-if (process.env.CMD_PREFIX) {
-  PREFIX = process.env.CMD_PREFIX;
+// Command prefix
+const PREFIX = process.env.CMD_PREFIX || "%";
+
+// Color
+const EMBED_COLOR = 0x7F00FF;
+
+// Voting duration
+const DURATION = 60;
+
+// Setup logger
+// https://www.npmjs.com/package/winston#usage
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
 }
-const ARROW_UP = "\u2b06";
-const ARROW_DOWN = "\u2b07";
 
-let destination = "Iowa";
-let ratio = 2.0;
+// Quests
+let quests = new Discord.Collection();
 
 // Event listener when a user connected to the server.
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  logger.info(`Logged in as ${client.user.tag}!`);
+  client.guilds.cache.forEach(guild => {
+    logger.info(`Adding a QuestManager for ${guild.name}!`);
+    quests.set(guild, new QuestBot.QuestManager(guild));
+  });
 });
 
-class Vote {
-  constructor(user, vote) {
-    this.user = user;
-    this.vote = vote;
-  }
+//////////////
+// Commands //
+//////////////
+
+function pong(message, args) {
+  message.reply('Pong!');
 }
 
-class Suggestion {
-  constructor(user, text) {
-    this.user = user;
-    this.text = text;
-    this.votes = new Array();
-    this.votes.push(new Vote(user, true));
-  }
-
-  upvote(msg) {
-    for (let i = 0; i < this.votes.length; i++) {
-      if (this.votes[i].user == msg.author) {
-        if (this.votes[i].vote) {
-          msg.reply("You already voted for that.");
-        } else {
-          this.votes[i].vote = true;
-          msg.reply("Changed your vote.");
-        }
-        return;
-      }
-    }
-    this.votes.push(new Vote(msg.author, true));
-  }
-
-  upvotes() {
-    var rtn = 0;
-    for (let i = 0; i < this.votes.length; i++) {
-      var vote = this.votes[i];
-      if (vote.vote) {
-        rtn++;
-      }
-    }
-    return rtn;
-  }
-
-  downvote(msg) {
-    for (let i = 0; i < this.votes.length; i++) {
-      if (this.votes[i].user == msg.author) {
-        if (!this.votes[i].vote) {
-          msg.reply("You already voted for that.");
-        } else {
-          this.votes[i].vote = false;
-          msg.reply("Changed your vote.");
-        }
-        return;
-      }
-    }
-    this.votes.push(new Vote(msg.author, false));
-  }
-
-  downvotes() {
-    var rtn = 0;
-    for (let i = 0; i < this.votes.length; i++) {
-      var vote = this.votes[i];
-      if (!vote.vote) {
-        rtn++;
-      }
-    }
-    return rtn;
-  }
-
-  accept() {
-    var rtn = false;
-    var up = this.upvotes();
-    var down = this.downvotes();
-    if (down == 0) {
-      down++;
-    }
-    if (up / down >= ratio) {
-      rtn = true;
-    }
-    return rtn;
-  }
-}
-
-var suggestions = new Array();
-
-var status = function(msg) {
-  var rtn = "\n";
-  var accepted = new Array();
-  for (let i = 0; i < suggestions.length; i++) {
-    let suggestion = suggestions[i];
-    let line = "`" + (i + 1).toString() + "`: ";
-    line += "   ";
-    line += ARROW_UP + " `";
-    line += suggestion.upvotes();
-    line += "` ";
-    line += ARROW_DOWN + " `";
-    line += suggestion.downvotes();
-    line += "`   (" + suggestion.user.username + ")   " + suggestion.text;
-    if (suggestion.accept()) {
-      line = "**" + line + "**";
-      accepted.push(encodeURIComponent(suggestion.text.replace(" ", "+")));
-    }
-    line += "\n";
-    rtn += line;
-  }
-  let link = "https://www.google.com/maps/dir/?api=1"
-  link += "&origin=";
-  // link += "34.1030032,-118.41046840000001";
-  link += "Current+Location";
-  link += "&destination=";
-  // link += "34.059808,-118.368152";
-  link += encodeURIComponent(destination);
-  if (accepted.length) {
-    link += "&waypoints=";
-    link += accepted.join("|");
-    // link += "34.072213,-118.399166|34.080318,-118.405343";
-  }
-  // rtn += link + "\n";
-  msg.reply(rtn);
-  const embed = new Discord.MessageEmbed()
-        .setTitle("Google Maps directions")
-        .setURL(link)
-        .setDescription("Driving directions from your current location to " + destination + ".")
-        .setTimestamp();
-  // const embed = new Discord.MessageEmbed().setTitle('Some title');
-  msg.channel.send(embed);
-}
-
-var getSuggestion = function(num) {
-  if (num > 0 && num <= suggestions.length) {
-    return suggestions[num - 1];
-  }
-}
-
-var upvote = function(msg, num) {
-  let suggestion = getSuggestion(num);
-  if (suggestion) {
-    suggestion.upvote(msg);
-  }
-}
-
-var downvote = function(msg, num) {
-  let suggestion = getSuggestion(num);
-  if (suggestion) {
-    suggestion.downvote(msg);
-  }
-}
-
-var addPoint = function(msg, point) {
-  suggestions.push(new Suggestion(msg.author, point));
-  msg.reply("Added waypoint '" + point + "'");
-}
-
-var delPoint = function(msg, num) {
-  let suggestion = getSuggestion(num);
-  if (suggestion && suggestion.user == msg.author) {
-    suggestions.splice(num - 1);
-    msg.reply("Deleted point `" + num + "`.");
-  }
-}
-
-var help = function(msg) {
+function help(message) {
   var help = "\n";
   help += `My prefix for commands is \`${PREFIX}\`\n`;
   help += `\n`;
   help += `**User commands**\n`;
-  help += `\`(${PREFIX}status | ${PREFIX}s)\` - Show waypoints and votes.\n`;
+  help += `> \`${PREFIX}quest DESCRIPTION\` - Start a quest for yourself.\n`;
+  help += `> \`${PREFIX}task USER LOCATION\` - Send user on a quest.\n`;
+  help += `> \`${PREFIX}status\` - Show active quests.\n`;
   // help += `\`${PREFIX}ping\` - reply 'Pong!'\n`;
-  help += `\`(${PREFIX}addpoint | ${PREFIX}a) POINT\` - Add waypoint, e.g. "mt rainier, wa".\n`;
-  help += `\`(${PREFIX}delpoint | ${PREFIX}b) NUMBER\` - Delete waypoint number \`NUMBER\`. You can only delete the point if you submitted it.\n`;
-  help += `\`(${PREFIX}upvote | ${PREFIX}u) NUMBER\` - Upvote waypoint.\n`;
-  help += `\`(${PREFIX}downvote | ${PREFIX}d) NUMBER\` - Downvote waypoint.\n`;
-  help += `\`(${PREFIX}help | ${PREFIX}h)\` - Display this message.\n`;
+  help += `> \`${PREFIX}help\` - Display this message.\n`;
   help += `\n`;
-  help += `**Admin commands**\n`;
-  help += `\`(${PREFIX}destination | ${PREFIX}x) DESTINATION\` - Set destination.\n`;
-  help += `\`(${PREFIX}ratio | ${PREFIX}r) RATIO\` - Set the vote ratio for accepting a waypoint. Default is 2.0.\n`;
   const embed = new Discord.MessageEmbed()
         .setTitle("Nav Bot Help")
         .setDescription(help)
-        .setColor(0x7F00FF)
-        .setTimestamp();
-  msg.channel.send(embed);
+        .setColor(EMBED_COLOR);
+  message.channel.send(embed);
 }
 
-var setDestination = function(msg, dest) {
-  destination = dest;
-  msg.reply("Setting destination to " + dest + ".");
+function getUserFromMention(mention, guild) {
+	const matches = mention.match(/^<@!?(\d+)>$/);
+	if (!matches) return;
+	const snowflake = matches[1];
+	return guild.members.fetch(snowflake);
 }
 
-var setRatio = function(r) {
-  ratio = r;
-}
-
-var isAdmin = function(msg) {
-  var rtn = false;
-  if (msg.member && msg.member.roles.cache.some(role => role.name === 'Admin')) {
-    rtn = true;
-  }
+function getActiveQuest(user, qm) {
+  let rtn = undefined;
+  qm.cache.forEach(quest => {
+    if (quest.user === user && quest.active) {
+      rtn = quest;
+    }
+  });
   return rtn;
 }
 
-// Event listener when a user sends a message in the chat.
-client.on('message', msg => {
-
-  if (msg.content.startsWith(PREFIX)) {
-    var args = msg.content.substring(PREFIX.length).split(' ');
-    var cmd = args[0];
-
-    args = args.splice(1);
-    switch(cmd) {
-    case 'ping':
-      msg.reply(msg.author);
-      msg.reply('Pong!');
-      break;
-    case 'addpoint':
-    case 'a':
-      var point = args.join(" ");
-      addPoint(msg, point);
-      break;
-    case 'delpoint':
-    case 'b':
-      if (args.length == 1) {
-        delPoint(msg, args[0]);
-      }
-      break;
-    case 'upvote':
-    case 'u':
-      if (args.length == 1) {
-        upvote(msg, args[0]);
-      }
-      break;
-    case 'downvote':
-    case 'd':
-      if (args.length == 1) {
-        downvote(msg, args[0]);
-      }
-      break;
-    case 'status':
-    case 's':
-      if (args.length == 0) {
-        status(msg);
-      }
-      break;
-    case 'help':
-    case 'h':
-      if (args.length == 0) {
-        help(msg);
-      }
-      break;
-    ////////////////////
-    // ADMIN COMMANDS //
-    ////////////////////
-    case 'destination':
-    case 'x':
-      if (isAdmin(msg)) {
-        setDestination(msg, args);
-      }
-      break;
-    case 'ratio':
-    case 'r':
-      if (isAdmin(msg)) {
-        if (args.length == 1) {
-          setRatio(args);
+async function createQuest(message, args) {
+  const description = args.join(' ');
+  const user = message.author;
+  if (!user) return;
+  const qm = quests.get(message.guild);
+  if (getActiveQuest(user, qm)) {
+    message.reply("You already have an active quest.");
+    return;
+  }
+  // Create embed
+  const embed = new Discord.MessageEmbed()
+        .setTitle(`🚶 New Quest`)
+        .setColor(EMBED_COLOR)
+        .setDescription(`${user} is starting a new quest!`)
+        .addField("Description", description || "???")
+        .addField("Agreement", `${user}, reacting to this message with a ✅ acts as consent to the terms of service of *And what about the truth* and Discord. We may quote and save your messages and may record your voice and video on this quest. You have ${DURATION} seconds to react or this quest will expire.`);
+  let reply = await message.channel.send(embed);
+  // Get reactions
+  reply.react('✅');
+  const filter = (reaction, user) => {
+	  return ['✅'].includes(reaction.emoji.name) && user.id === message.author.id;
+  };
+  reply.awaitReactions(filter, { max: 1, time: DURATION*1000, errors: ['time'] })
+	  .then(collected => {
+		  const reaction = collected.first();
+		  if (reaction.emoji.name === '✅') {
+        if (getActiveQuest(user, qm)) {
+          message.reply("You already have an active quest.");
+        } else {
+			    message.reply("Your quest is confirmed.");
+          const snowflake = Discord.SnowflakeUtil.generate();
+          const quest = new QuestBot.Quest(client, {id: snowflake, user: user, description: description, message: reply});
+          qm.add(quest);
         }
-      }
-      break;
-    }
+		  }
+	  })
+	  .catch(collected => {
+      console.log(collected);
+      embed.setTitle("~~" + embed.title + "~~ (expired)");
+      reply.edit(embed);
+	  });
+}
+
+async function sendUserOnTask(message, args) {
+  // Create embed
+  const mention = args[0];
+  if (!mention) return;
+  const user = await getUserFromMention(mention, message.guild);
+  if (!user) return;
+  const destination = args.splice(1).join(' ');
+  if (!destination) return;
+  const google = "https://maps.google.com?q=" + destination.replace(' ', '+');
+  const embed = new Discord.MessageEmbed()
+        .setTitle(`☑️ New Task`)
+        .setColor(EMBED_COLOR)
+        .setDescription(`${message.author} is trying to start a new task for ${user}! Voting will end in ${DURATION} seconds...`)
+        .addField("Destination", `[${destination}](${google})`);
+  let reply = await message.channel.send(embed);
+  // Get reactions
+  reply.react('👍').then(() => reply.react('👎'));
+  const filter = (reaction, user) => {
+	  return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
+  };
+  reply.awaitReactions(filter, { max: 1, time: DURATION*1000, errors: ['time'] })
+	  .then(collected => {
+		  const reaction = collected.first();
+		  if (reaction.emoji.name === '👍') {
+			  reply.reply('you reacted with a thumbs up.');
+        // let task = new QuestBot.Task(client, {id: Discord.SnowflakeUtil.generate()});
+		  } else {
+			  reply.reply('you reacted with a thumbs down.');
+		  }
+	  })
+	  .catch(collected => {
+		  reply.reply('you reacted with neither a thumbs up, nor a thumbs down.');
+	  });
+}
+
+async function status(message, args) {
+  const qm = quests.get(message.guild);
+  let status = "";
+  qm.cache.forEach(quest => {
+    status += `  • (${quest.user}) ` + (quest.description || "???") + "\n";
+  });
+  if (status === "") status = "*No active quests.*";
+  const embed = new Discord.MessageEmbed()
+        .setTitle(`Active Quests`)
+        .setColor(EMBED_COLOR)
+        .setDescription(status);
+  message.channel.send(embed);
+}
+
+// Event listener when a user sends a message in the chat.
+client.on('message', message => {
+
+  if (!message.content.startsWith(PREFIX)) return;
+
+  var args = message.content.substring(PREFIX.length).split(' ');
+  var cmd = args[0];
+  args = args.splice(1);
+
+  switch(cmd) {
+  case 'ping':
+    pong(message, args);
+    break;
+  case 'quest':
+  case 'q':
+    createQuest(message, args);
+    break;
+  case 'task':
+  case 't':
+    sendUserOnTask(message, args);
+    break;
+  case 'status':
+  case 's':
+    status(message, args);
+    break;
+  case 'help':
+  case 'h':
+    help(message);
+    break;
   }
 });
 
